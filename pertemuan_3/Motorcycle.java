4c98b698701d42d8076becb7c98727812f89dfed
Rayzan F.R. <rayzan935@gmail.com>
2025-10-30 14:33:37 +0700

    mengganti file kt menjadi java

---
+++ pertemuan_3/Motorcycle.java
@@ -0,0 +1,16 @@
+class Motorcycle implements VehicleInterface {
+    @Override
+    public void drive() {
+        System.out.println("Motorcycle is driving");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Motorcycle stopped");
+    }
+
+    @Override
+    public void refuel() {
+        System.out.println("Motorcycle is refueling");
+    }
+}
--- pertemuan_3/Motorcycle.kt
+++
@@ -1,5 +0,0 @@
-class Motorcycle : VehicleInterface {
-override fun drive() {}
-override fun stop() {}
-override fun refuel() {}
-}
